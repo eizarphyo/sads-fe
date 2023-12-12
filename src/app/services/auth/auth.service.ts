67fe0161812922c21a54814bb06ca3fa6a76c9f7
Woody <waiyanwoody@gmail.com>
2023-12-12 21:46:03 +0630

    cart list ui fixed

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { apiHost } from 'src/utils/utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+
+  ) { }
+
+  login(user: any) {
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     "Content-Type": "application/json",
+    //     "Access-Control-Allow-Origin": "*"
+
+    //   })
+    // }
+    return new Promise((resolve, reject) =>  {
+      this.http.post(`${apiHost}/login`, user).subscribe({
+
+        next: (res: any) => {
+        console.log(res.statusCode);
+          resolve(res);
+        }, error: (error: any) => {
+          console.log('>>>>', error);
+
+          reject(error);
+        }
+    })
+    } )
+
+  }
 }
---
+++ src/utils/utils.ts
@@ -0,0 +1,1 @@
+export var apiHost = 'http://127.0.0.1/api';
