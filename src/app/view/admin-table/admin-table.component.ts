5742df9d14ba79b9ba05b067866b9a0fbc718438
Kaung Pyae Min Thein <kaungpyae.kg@gmail.com>
2023-12-13 16:55:23 +0630

    finish GUI tables

--- src/app/view/admin-table/admin-table.component.ts
+++ src/app/view/admin-table/admin-table.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+
+const ADMIN_DATA: any[] = [
+  {
+    no: 1,
+    date: "2023-01-01",
+    customerName: "John Doe",
+    preorderNo: "PO12345",
+    region: "North",
+    totalQty: 100,
+    totalAmount: 500
+  },
+  {
+    no: 2,
+    date: "2023-02-05",
+    customerName: "Rosey",
+    preorderNo: "PO67890",
+    region: "South",
+    totalQty: 75,
+    totalAmount: 500
+  },
+  {
+    no: 3,
+    date: "2023-03-10",
+    customerName: "David",
+    preorderNo: "PO54321",
+    region: "West",
+    totalQty: 120,
+    totalAmount: 230
+  }
+];
 @Component({
   selector: 'app-admin-table',
   templateUrl: './admin-table.component.html',
   styleUrls: ['./admin-table.component.css']
 })
 export class AdminTableComponent {
+  myAdminData: any = [];
+  displayedColumns: string[] = ['no', 'date', 'customerName', 'preorderNo', 'region', 'totalQty', 'totalAmount']
 
+  dataSource = new MatTableDataSource(ADMIN_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+  }
 }
--- src/app/view/factory-table/factory-table.component.ts
+++ src/app/view/factory-table/factory-table.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+const FACTORY_DATA: any[] = [
+  {
+    no: 1,
+    date: "2023-01-01",
+    item: "Tea",
+    totalQty: 100,
+    box: 200
+  },
+  {
+    no: 2,
+    date: "2023-02-05",
+    item: "Juice",
+    totalQty: 75,
+    box: 500,
+  },
+  {
+    no: 3,
+    date: "2023-03-10",
+    item: "Liquor",
+    totalQty: 120,
+    box: 3
+  }
+];
 @Component({
   selector: 'app-factory-table',
   templateUrl: './factory-table.component.html',
   styleUrls: ['./factory-table.component.css']
 })
 export class FactoryTableComponent {
+  myLogisticData: any = [];
+  displayedColumns: string[] = ['no', 'date', 'item', 'totalQty', 'box'];
 
+  dataSource = new MatTableDataSource(FACTORY_DATA);
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+  }
 }
